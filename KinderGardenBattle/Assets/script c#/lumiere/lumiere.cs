997c82e0b8c6759fa9a29b87a6391cd409dfa437
maximilien <maximilien.moriquand@epita.fr>
2019-02-28 18:26:00 +0100

    grosse base du projet
    Le mouvement du perso est fait , -> manque la vrai map  avec les cases .

---
+++ KinderGardenBattle/Assets/script c#/lumiere/lumiere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lumiere : MonoBehaviour
+{
+    
+    public Light Lumiere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetButtonDown("space"))
+        {
+            Lumiere.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
