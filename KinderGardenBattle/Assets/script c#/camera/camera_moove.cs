b2698369064a5c9fccd74f28ff61fbb2a5292984
maximilien <maximilien.moriquand@epita.fr>
2019-03-03 17:47:43 +0100

    Fucking camera finie
    8 et 2 => dezoome
    4 et 6 => change de point de vue

--- KinderGardenBattle/Assets/script c#/camera/camera_moove.cs
+++ KinderGardenBattle/Assets/script c#/camera/camera_moove.cs
@@ -6,43 +6,83 @@ using Vector3 = UnityEngine.Vector3;
 
 public class camera_moove : MonoBehaviour
 {
-  public Transform target;
-  public float vitesse_cam=0.125f;
-    private Vector3 offset = new Vector3(10, 10, 10);
-
+    public Transform target;
+    public float vitesse_cam = 0.125f;
+    public Vector3 offset = new Vector3(20, 20, 20);
+    public  int cas = 0;
     private void Update()
     {
-       // if (Input.GetKeyDown(KeyCode.O))
-       // {
-      //     offset.y += 5;
-       // }
+        cas = (Input.GetKeyDown(KeyCode.Keypad6) ? ++cas : (Input.GetKeyDown(KeyCode.Keypad4) ? --cas : cas))%4;
+        if (cas < 0)
+            cas += 4;
 
-       // if (Input.GetKeyDown(KeyCode.L))
-        //{
-           // offset.y -= 5;
-       // }
-
-        if (Input.GetKeyDown(KeyCode.M))
+        if (cas == 0)
         {
-            offset.x += 5;
+            if (offset.x < 0)
+               offset.x= offset.x * (-1);
+            if (offset.z < 0)
+                offset.z = offset.z * (-1);
+
         }
 
-        if (Input.GetKeyDown(KeyCode.K))
+        if (cas == 1)
         {
-            offset.x -= 5;
+            if (offset.x < 0)
+                offset.x=offset.x * (-1);
+            if (offset.z>0)
+            {
+                offset.z = offset.z * (-1);
+            }
+
+        }
+
+        if (cas == 2)
+        {
+            if (offset.x > 0)
+            offset.x= offset.x * (-1);
+            if (offset.z > 0)
+                offset.z = offset.z * (-1);
+
+        }
+
+        if (cas == 3)
+        {
+            if (offset.x > 0)
+            offset.x= offset.x * (-1);
+            if (offset.z < 0)
+                offset.z = offset.z * (-1);
+
+        }
+        
+        if (offset.y < 45)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad8))
+            {
+                offset.y += 5;
+                offset.x += offset.x < 0 ? -5 : 5;
+                offset.z += offset.z < 0 ? -5 : 5;
+            }
+        }
+        if (offset.y > 15)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                offset.y -= 5;
+                offset.x += offset.x < 0 ? 5 : -5;
+                offset.z += offset.z < 0 ? 5 : -5;
+            }
+            
         }
     }
 
     private void LateUpdate()
     {
-       
+
         Vector3 Position_ = target.position + offset;
         Vector3 Nposition = Vector3.Lerp(transform.position, Position_, vitesse_cam);
         transform.position = Nposition;
         transform.LookAt(target);
     }
-    
-    
-
 }
+
     
