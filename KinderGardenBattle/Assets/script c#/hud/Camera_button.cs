d933f435a44e73e15f484313457c33ade74f11d5
adrien houpert <adrien.houpert@epita.fr>
2019-03-03 19:11:46 +0100

    j'ai ajoute tous les boutons mais faudra faire les fonctions pour la camera.

---
+++ KinderGardenBattle/Assets/script c#/hud/Camera_button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class Camera_button : MonoBehaviour
+{
+    public void set_cas_left()
+    {
+    }
+    
+    public void set_cas_right()
+    {
+        
+    }
+
+    public void zoom_plus()
+    {
+        
+    }
+
+    public void zomm_less()
+    {
+        
+    }
+}
