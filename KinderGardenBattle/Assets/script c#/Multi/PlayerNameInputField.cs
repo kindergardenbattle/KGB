9694b6b897c94a209aca5d891e975cef0a46c945
unknown <adrien.houpert@epita.fr>
2019-03-10 11:53:57 +0100

    oui

--- KinderGardenBattle/Assets/script c#/Multi/PlayerNameInputField.cs
+++ KinderGardenBattle/Assets/script c#/Multi/PlayerNameInputField.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-using Photon.Realtime;
-using System.Collections;
 
 /// <summary>
 /// Player name input field. Let the user input his name, will appear above the player in the game.
