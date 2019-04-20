78b37988afa65b59e1e37d52873192617311ddf3
unknown <adrien.houpert@epita.fr>
2019-04-20 08:50:12 +0200

    premier test de la journee

--- KinderGardenBattle/Assets/script c#/Multi/Generale_Attaque_multi.cs
+++ KinderGardenBattle/Assets/script c#/Multi/Generale_Attaque_multi.cs
@@ -9,38 +9,6 @@ public class Generale_Attaque_multi : TacticsMove_multi
     public bool bascule2 = true;
     public bool Want_to_fight = false;
 
-    public void GetTarget(int atk)
-    {
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            Debug.Log("debut get target");
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 25.0f))
-            {
-                if (hit.transform != null && hit.transform.gameObject.CompareTag("NPC"))
-                {
-                    GameObject npc = hit.transform.gameObject;
-                    //manager_cible= npc.GetComponent<GameManagerSolo>();
-                    Perso_Generique cara_cible = npc.GetComponent<Perso_Generique>();
-                    //cara_cible.SetClasse(cara_cible.classe);
-                    Debug.Log(cara_cible.ClasseToString());
-                    //Debug.Log("cible acquise :" +cara_cible.ClasseToString());
-                    Debug.Log(cara_cible.Hp);
-                    cara_cible.NewPV(atk);
-                    Debug.Log(cara_cible.Hp);
-                    //return cara_cible;
-                }
-            }
-            //return null;
-        }
-        //return null;
-
-
-
-
-    }
     public Perso_Generique SelectionPerso(bool boolen)
     {
 
--- KinderGardenBattle/Assets/script c#/Multi/Player_manager_multi.cs
+++ KinderGardenBattle/Assets/script c#/Multi/Player_manager_multi.cs
@@ -45,4 +45,31 @@ public class Player_manager_multi : Generale_Attaque_multi
         if (moving)
             Move();
     }
+
+    public void GetTarget(int atk)
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("debut get target");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 25.0f))
+            {
+                if (hit.transform != null && hit.transform.gameObject.CompareTag("Player"))
+                {
+                    GameObject npc = hit.transform.gameObject;                    //manager_cible= npc.GetComponent<GameManagerSolo>();
+                    Player_manager_multi ennemi = npc.GetComponent<Player_manager_multi>();
+                    if (ennemi.is_turn!=is_turn)
+                    { 
+                        Perso_Generique cara_cible = npc.GetComponent<Perso_Generique>();                    //cara_cible.SetClasse(cara_cible.classe);
+                        Debug.Log(cara_cible.ClasseToString());                    //Debug.Log("cible acquise :" +cara_cible.ClasseToString());
+                        Debug.Log(cara_cible.Hp);
+                        cara_cible.NewPV(atk);
+                        Debug.Log(cara_cible.Hp);                    //return cara_cible;
+                    }
+                }
+            }            //return null;
+        }        //return null;
+    }
 }
