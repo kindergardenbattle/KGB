ef4cedaeecf44f75696cf182a0a05a8fed6709b2
unknown <adrien.houpert@epita.fr>
2019-05-28 09:44:42 +0200

    premierpush

---
+++ KinderGardenBattle/Assets/script c#/2.0lemulti/GameManagermulti.cs
@@ -0,0 +1,255 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManagermulti : MonoBehaviourPunCallbacks
+{
+    public static bool turn = true;//synchronise the turn with the master client
+    private static bool ancien_turn = turn;
+    public static bool Move_Button;
+    public static bool ATK_Button;
+    public static bool is_in_menu = false;
+    public static bool want_to_change_classe = false;
+    public Perso_Generiquemulti vis;
+    public GameObject Stat;
+    public static bool has_change_classe = false;
+    public static int futur_classe;
+
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from");
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            float a = PhotonNetwork.CurrentRoom.PlayerCount == 1 ? 0f : -5f;
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(a, 2f, a), Quaternion.identity, 0);
+        }
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (go.CompareTag("Button"))
+                go.SetActive(PhotonNetwork.IsMasterClient ? turn : !turn);//go.activeSelf
+        }
+        Move_Button = false;
+        ATK_Button = false;
+        TeamTurn = Team.Blue;
+        J1 = true;
+        J2 = J3 = J4 = false;        
+    }
+
+    [PunRPC]
+    public void Set_turn() { photonView.RPC("Lel", RpcTarget.All); }
+
+    [PunRPC]
+    void Lel() { turn = !turn; }
+
+    public enum Team
+    {
+        Blue, Red
+    }
+
+    public static bool J1;
+    public static bool J2;
+    public static bool J3;
+    public static bool J4;
+    
+    public void QuelJoueur()
+    {
+        if (J1)
+        {
+            J1 = J2;
+            J2 = true;
+        }
+
+        else
+        {
+            if (J2)
+            {
+                J2 = !J2;
+                J3 = true;
+            }
+            else
+            {
+                if (J3)
+                {
+                    J3 = !J3;
+                    J4 = true;
+                }
+                else
+                {
+                    J4 = false;
+                    J1 = true;
+                }
+            }
+        }
+
+    }
+    public void setfuturclasse(int i)
+    {
+        futur_classe = i;
+    }
+
+    public void confirmclasse()
+    {
+        has_change_classe = true;
+    }
+    public static bool NomDuJoeurVersUneBool(string name)
+    {
+        switch (name)
+        {
+            case "plamulti(Clone)":
+                return J1;
+            case "2":
+                return J2;
+            case "3":
+                return J3;
+            case "4":
+                return J4;
+            default:
+                return false;
+
+        }
+    }
+    public static string TeamtoString(Team team)
+    {
+        switch (team)
+        {
+            case Team.Blue:
+                return "BLEU";
+            case Team.Red:
+                return "RED";
+            default:
+                return "ERROR";
+        }
+    }
+
+    public static Team TeamTurn;
+    public static bool booleen = false;
+
+    public void Button_Moove()
+    {
+
+        Move_Button = !Move_Button;
+        ATK_Button = false;
+        Debug.Log("MOVE " + Move_Button);
+        Stat.SetActive(false);
+    }
+    public void Button_ATK()
+    {
+        ATK_Button = !ATK_Button;
+        Move_Button = false;
+        Debug.Log("ATK" + ATK_Button);
+        Stat.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            menu();
+        }
+        if (!Move_Button && !ATK_Button)
+        {
+            show_stat();
+        }
+        if (turn != ancien_turn)
+        {
+            FinDeTour();
+            ancien_turn = turn;
+        }
+    }
+
+    public void show_stat()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 25.0f))
+            {
+                if (hit.transform != null && (hit.transform.gameObject.CompareTag("Player") || hit.transform.gameObject.CompareTag("NPC")))
+                {
+                    GameObject npc = hit.transform.gameObject;                    //manager_cible= npc.GetComponent<GameManagerSolo>();
+                    vis = npc.GetComponent<Perso_Generiquemulti>();
+                    Stat.SetActive(true);
+                    Player_Statsmulti s = Stat.GetComponent<Player_Statsmulti>();
+                    s.set_player_stats(vis);
+                }
+                else
+                {
+                    if (hit.transform != null && hit.transform.gameObject.CompareTag("Box"))
+                    {
+                        menuselectionclasse();
+                        Debug.Log("sortie de fonction");
+                    }
+                }
+            }
+        }
+    }
+
+    public void menuselectionclasse()
+    {
+        Debug.Log("entree");
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (go.CompareTag("Button"))
+                go.SetActive(want_to_change_classe);//go.activeSelf
+            if (go.CompareTag("ChangeClasse"))
+                go.SetActive(!want_to_change_classe);
+        }
+        Stat.SetActive(false);
+        want_to_change_classe = !want_to_change_classe;
+    }
+
+    public void menu()
+    {
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            Debug.Log("change clase" + want_to_change_classe);
+            if (go.CompareTag("Button"))
+                go.SetActive(is_in_menu ? !want_to_change_classe : is_in_menu);//go.activeSelf
+            if (go.CompareTag("Menupause"))
+                go.SetActive(!is_in_menu);
+            if (go.CompareTag("ChangeClasse"))
+                go.SetActive(is_in_menu ? want_to_change_classe : is_in_menu);
+
+        }
+        Stat.SetActive(false);
+        is_in_menu = !is_in_menu;
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+       
+    public static void SetTeamTurn(Team Color)
+    {
+        TeamTurn = Color;
+    }
+
+    public void FinDeTour()
+    {
+        TeamTurn = (TeamTurn == Team.Blue) ? Team.Red : Team.Blue;
+        Move_Button = false;
+        ATK_Button = false;
+        Stat.SetActive(false);
+        Debug.Log("Tours de l'equipe : " + TeamtoString(TeamTurn));
+
+        GameObject[] liste = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject VARIABLE in liste)
+        {
+            PlayerMovemulti script = VARIABLE.GetComponent<PlayerMovemulti>();
+            script.hasmooved = false;
+        }
+    }
+}
---
+++ KinderGardenBattle/Assets/script c#/2.0lemulti/PlayerMovemulti.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class PlayerMovemulti : TacticsMove
+{
+    private bool la_seboul;
+    public GameObject CibleRotate;
+    public bool cible;
+    public bool hasmooved;
+
+    void Rotate()
+    {
+        GameObject[] listperso = GameObject.FindGameObjectsWithTag("NPC");
+        foreach (GameObject VARIABLE in listperso)
+        {
+            if ((this.transform.position.x - VARIABLE.transform.position.x < 1.5 &&
+                 transform.position.z - VARIABLE.transform.position.z < 1.5) &&
+                (this.transform.position.x - VARIABLE.transform.position.x > -1.5 &&
+                 transform.position.z - VARIABLE.transform.position.z > -1.5))
+            {
+                //Debug.Log("oui");
+                CibleRotate = VARIABLE;
+                transform.LookAt(CibleRotate.transform);
+            }
+        }
+        //Debug.Log("le joueur: "+this.name+" a rotate");
+
+    }
+
+    void Start()
+    {
+        Init();
+        la_seboul = false;
+        hasmooved = false;
+        CibleRotate = null;
+    }
+
+    public void button()
+    {
+
+        la_seboul = !la_seboul;
+    }
+
+    void Update()
+    {
+        Debug.Log(gameObject.name);
+        if (GameManagermulti.TeamTurn == GameManagermulti.Team.Red)
+        {
+            Rotate();
+            GetCurrentTile();
+        }
+        
+        if (GameManagermulti.NomDuJoeurVersUneBool(gameObject.name))
+        {
+            if (GameManagermulti.has_change_classe)
+            {
+                hasmooved = true;
+                gameObject.GetComponent<Perso_Generiquemulti>().int_to_classe(GameManagermulti.futur_classe);
+                GameManagermulti.has_change_classe = false;
+            }
+
+            if (!GameManagermulti.Move_Button && !GameManagermulti.ATK_Button)
+            {
+
+                Resetalltiles();
+                GetCurrentTile();
+            }
+
+            if (GameManagermulti.TeamTurn == GameManagermulti.Team.Red)
+            {
+                Rotate();
+            }
+
+            if (hasmooved == false)
+            {
+
+
+                Debug.DrawRay(transform.position, transform.forward);
+
+
+
+                anim.SetBool("Déplacement", moving);
+                if (!moving)
+                {
+
+                    if (GameManagermulti.Move_Button && !GameManagermulti.ATK_Button)
+                    {
+                        FindSelectableTiles(gameObject.GetComponent<Perso_Generiquemulti>().Pm); //appelle les fonction si ça bouge pas 
+                        CheckMouse();
+                        Rotate();
+                    }
+                }
+
+                if (!moving && DebutTour == false)
+                {
+                    hasmooved = true;
+                    Resetalltiles();
+                    DebutTour = true;
+                }
+            }
+
+            if (!GameManagermulti.Move_Button && GameManagermulti.ATK_Button)
+            {
+                Rotate();
+                FindSelectableTiles(gameObject.GetComponent<Perso_Generiquemulti>().Distance);
+            }
+
+        }
+
+        else
+        {
+            GetCurrentTile();
+            Rotate();
+            return;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (!GameManagermulti.Move_Button || GameManagermulti.TeamTurn == GameManagermulti.Team.Red)
+        {
+            moving = false;
+            return;
+        }
+        if (moving)
+        {
+            Move();
+            DebutTour = false;
+        }
+
+
+    }
+
+    void CheckMouse()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Tile") // verifie que l'on tape une case 
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+
+                    if (t.selectable)
+                    {
+                        MoveToTile(t);
+                    }
+                }
+            }
+        }
+    }
+}
+
